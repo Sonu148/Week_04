68b13d385579adc4c28ca9cb74dd0f5a9cd03353
Sonu148 <sonukumar31815@gmail.com>
2025-02-14 00:22:55 +0530

    [Sonu]Add. find the key with the highest value

---
+++ Map/MaxValueKey.java
@@ -0,0 +1,11 @@
+package com.day_02.Map;
+import java.util.*;
+
+public class MaxValueKey {
+    public static void main(String[] args) {
+        Map<String, Integer> map = Map.of("A", 10, "B", 20, "C", 15);
+
+        String maxKey = Collections.max(map.entrySet(), Map.Entry.comparingByValue()).getKey();
+        System.out.println(maxKey);  // Output: B
+    }
+}
